9390c3a31e6d6f9991681ddd84956a39ad216925
imehc <richohealthy@hotmail.com>
2024-08-06 10:15:08 +0800

    perf(server): 优化类型定义

--- meeting_room_booking_system_backend/src/config/configuration-impl.ts
+++ meeting_room_booking_system_backend/src/config/configuration-impl.ts
@@ -1,46 +1,30 @@
-export interface ConfigurationImpl {
-  ['redis-server.host']: string;
-  ['redis-server.port']: number;
-  ['redis-server.db']: number;
+type baseKeys = 'host' | 'port';
+type RedisConfig = `redis-server.${baseKeys | 'db'}`;
+type MysqlConfig =
+  `mysql-server.${baseKeys | 'username' | 'password' | 'database'}`;
+type MinioConfig =
+  `minio-server.${'endpoint' | 'port' | 'access-key' | 'secret-key' | 'bucket-name' | 'expires'}`;
+type NodemailerConfig = `nodemailer-server.${baseKeys | 'user' | 'pass'}`;
+type WinstonConfig =
+  `winston.log-${'level' | 'dirname' | 'filename' | 'date-pattern' | 'max-size'}`;
+type GoogleLoginConfig =
+  `google.login.${'client-id' | 'client-secret' | 'callback-url' | 'redirect-url'}`;
+type GithubLoginConfig =
+  `github.login.${'client-id' | 'client-secret' | 'callback-url' | 'redirect-url'}`;
 
-  ['mysql-server.host']: string;
-  ['mysql-server.port']: number;
-  ['mysql-server.username']: string;
-  ['mysql-server.password']: string;
-  ['mysql-server.database']: string;
+type JwtConfig =
+  `jwt.${'secret' | 'access-token-expires-time' | 'access-refresh-expires-time'}`;
+type NestServerConfig = `nest-server.${'port' | 'doc-url'}`;
 
-  ['minio-server.endpoint']: string;
-  ['minio-server.port']: number;
-  ['minio-server.access-key']: string;
-  ['minio-server.secret-key']: string;
-  ['minio-server.bucket-name']: string;
-  ['minio-server.expires']: string;
-
-  ['nodemailer-server.host']: string;
-  ['nodemailer-server.port']: number;
-  ['nodemailer-server.user']: string;
-  ['nodemailer-server.pass']: string;
-
-  ['winston.log-level']: string;
-  ['winston.log-dirname']: string;
-  ['winston.log-filename']: string;
-  ['winston.log-date-pattern']: string;
-  ['winston.log-max-size']: string;
-
-  ['google.login.client-id']: string;
-  ['google.login.client-secret']: string;
-  ['google.login.callback-url']: string;
-  ['google.login.redirect-url']: string;
-
-  ['github.login.client-id']: string;
-  ['github.login.client-secret']: string;
-  ['github.login.callback-url']: string;
-  ['github.login.redirect-url']: string;
-
-  ['jwt.secret']: string;
-  ['jwt.access-token-expires-time']: string;
-  ['jwt.access-refresh-expires-time']: string;
-
-  ['nest-server.port']: number;
-  ['nest-server.doc-url']: string;
-}
+export declare type ConfigurationImpl = {
+  [K in
+    | RedisConfig
+    | MysqlConfig
+    | MinioConfig
+    | NodemailerConfig
+    | WinstonConfig
+    | GoogleLoginConfig
+    | GithubLoginConfig
+    | JwtConfig
+    | NestServerConfig]: string;
+};
