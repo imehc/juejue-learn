99220eebaf563151c50433e7e61cf9f13b8200a9
imehc <richohealthy@hotmail.com>
2024-09-11 14:31:22 +0800

    fix：多余执行步骤

--- .github/scripts/frontend-setup.sh
+++ .github/scripts/frontend-setup.sh
@@ -3,7 +3,6 @@
 cd ./meeting_room_booking_system_frontend
 pnpm install
 pnpm dlx @openapitools/openapi-generator-cli generate -g typescript-fetch -c ./openapi-generator.config.yaml -o ./meeting-room-booking-api -i ../meeting_room_booking_system_backend/openapi.yaml
-pnpm test
 pnpm build
 # PORT=6022 node .next/standalone/server.js
 kill $(lsof -t -i:6022) || true
