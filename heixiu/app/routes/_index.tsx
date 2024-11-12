59c3d2e21fa451aa0f6ce8d66f7746b8f789188e
imehc <richohealthy@hotmail.com>
2024-11-12 11:45:55 +0800

    feat: init

---
+++ heixiu/app/routes/_index.tsx
@@ -0,0 +1,33 @@
+import { invoke } from "@tauri-apps/api/core";
+import { useState } from "react";
+import { Button } from "~/components/ui/button";
+
+export default function Index() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return (
+    <div>
+      <form
+        className="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          greet();
+        }}
+      >
+        <input
+          id="greet-input"
+          onChange={(e) => setName(e.currentTarget.value)}
+          placeholder="Enter a name..."
+        />
+        <Button type="submit">Greet</Button>
+      </form>
+      <p>{greetMsg}</p>
+    </div>
+  );
+}
